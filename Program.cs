505b824513dd54b5ae99f35d107b94c8103e2819
Davi Jose Batista lima de Oliveria <davibatistalimadeoliveria@gmail.com>
2022-10-28 14:43:00 -0300

    fluxograma da engenharia concluido

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+Console.WriteLine("   Fluxograma Da Engenharia    \n");
+
+Console.Write("O objeto esta se movendo S/N?");
+
+bool EstaSeMovendo = Console.ReadLine()!.Trim().Substring(0,1).ToUpper() == "S";
+
+Console.Write("deveria?");
+
+bool Deveria = Console.ReadLine()!.Trim().Substring(0,1).ToUpper() == "S";
+
+string Mensagem;
+
+if (!EstaSeMovendo)
+{
+
+    if (!Deveria)
+
+    {
+        Mensagem = "ótimo!";
+
+    }
+
+    else
+
+    {
+
+        Mensagem = "Use WD-40.";
+
+    }
+
+}
+
+else
+
+{
+      if (Deveria)
+
+     {
+
+        Mensagem = "Ótimo!";
+
+    }   
+
+    else
+
+     {
+
+        Mensagem = "Use Silver Tape.";
+
+     }
+
+}
+
+Console.WriteLine();
+
+Console.WriteLine(Mensagem);
